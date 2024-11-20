eb9d89f29572b8302c2fb9cb7de1e22e2025467c
Kurisu996 <florian.schmidt0410@gmail.com>
2024-11-20 16:53:17 +0100

    Background Added to Gameplay

--- Assets/Scenes/Schmidt/Scripts/MainMenu.cs
+++ Assets/Scenes/Schmidt/Scripts/MainMenu.cs
@@ -3,6 +3,13 @@ using UnityEngine.SceneManagement;
 
 public class MainMenu : MonoBehaviour
 {
+    public Texture2D cursorTexture;
+    public CursorMode cursorMode = CursorMode.ForceSoftware;
+    public Vector2 hotSpot = Vector2.zero;
+    void Start()
+    {
+        Cursor.SetCursor(cursorTexture, hotSpot, cursorMode);
+    }
     public void PlayGame()
     {
         SceneManager.LoadScene("Assets/Scenes/Schmidt/SchmidtTestRange.unity");
