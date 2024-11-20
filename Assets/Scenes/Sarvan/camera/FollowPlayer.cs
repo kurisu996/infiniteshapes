33d794bd0eb06a54d01b836b06ab040e32014f1d
jigbre <jana0tuerlich@gmail.com>
2024-11-20 09:54:18 +0100

    will continue later

--- Assets/Scenes/Sarvan/camera/FollowPlayer.cs
+++ Assets/Scenes/Sarvan/camera/FollowPlayer.cs
@@ -1,6 +1,7 @@
 using UnityEngine;
 
-public class FollowPlayer : MonoBehaviour {
+public class FollowPlayer : MonoBehaviour{
+    [SerializeField] public bool corrupted = false;
     void LateUpdate() {
         transform.rotation = Quaternion.Euler(0f, 0f, 0f);
     }
