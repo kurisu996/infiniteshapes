235810a8d6930b65872cdcde0402b96ae43ca8bb
xXSoupNoobXx <3201@htl.rennweg.at>
2024-12-16 18:00:48 +0100

    Darko scene rework
    Movement und kamera funktionieren jz

---
+++ Assets/Scenes/Ljubobrativic/scripts/irgendeinname.cs
@@ -0,0 +1,15 @@
+using UnityEngine;
+
+public class irgendeinname : MonoBehaviour
+{
+    [SerializeField] Transform target; // Setze dein Spieler-Objekt hier rein
+    [SerializeField] Vector3 offset = new Vector3(0, 0, -10); // Kamera-Offset
+
+    private void LateUpdate()
+    {
+        if (target != null)
+        {
+            transform.position = target.position + offset; // Folgt dem Spieler
+        }
+    }
+}
